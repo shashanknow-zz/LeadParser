903a6df0cfd0233e7945a3956a50313fc3d7e0f7
Shashank Kunikullaya <shashank.ubk@gmail.com>
2020-06-29 01:16:09 +0530

    Update TestCase.py

--- LeadParser/Test/TestCase.py
+++ LeadParser/Test/TestCase.py
@@ -4,9 +4,9 @@ import os
 with open('Correct.json') as f:
 	correct_data = json.load(f)
 
-test_data_path = (os.path.dirname(os.getcwd()) + '\\Output')
+test_data_path = (os.path.dirname(os.getcwd()) + '/Output')
 
-with open(test_data_path + "\\" + "output.json") as f2:
+with open(test_data_path + "/" + "output.json") as f2:
 	test_data = json.load(f2)
 
 if(correct_data == test_data):
